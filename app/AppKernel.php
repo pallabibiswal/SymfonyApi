760a6409a74385731ba422741cf86e310e71f416
pallabi <pallabi.biswal@mindfiresolutions.com>
2018-07-17 14:53:19 +0530

    Install ForRest,DoctrineMigration and serializer bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
